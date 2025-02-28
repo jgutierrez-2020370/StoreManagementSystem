7eca3d8000ce6d37c3cc26da2fbdd94c1014903b
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-27 22:54:50 -0600

    admin default and role change function added

--- src/user/user.admin.controller.js
+++ src/user/user.admin.controller.js
@@ -1,27 +1,90 @@
 import User from '../user/user.model.js'
 import { encrypt } from '../../utils/encrypt.js'
+import Cart from '../shoppingCart/shoppingCart.model.js'
 
-export const registerAdmin = async(req, res) => {
+export const defaultAdmin = async(req, res) => {
     try {
-        let data = req.body
-        let user = new User(data)
-        user.password = await encrypt(user.password)
-        user.role = 'ADMIN'
-        await user.save()
-        return res.send(
-            {
-                success: true,
-                message: `Admin registed succesfully, can be logged with username: ${user.username}`
-            }
-        )
+        const defautlAdmin = await User.findOne({name: 'Admin'})
+
+        if (!defautlAdmin) {
+            const carrito = new Cart(
+                {
+                    items: [],
+                    total: 0,
+                }
+            )
+            await carrito.save()
+
+            const newADmin = new User(
+                {
+                    name: 'Admin',
+                    surname: 'Admin',
+                    username: 'Admin',
+                    email: process.env.ADMIN_EMAIL,
+                    password: await encrypt(process.env.ADMIN_PASS),
+                    role: 'ADMIN',
+                    cart: carrito._id
+                }
+            )
+            await newADmin.save()
+        }
     } catch (err) {
         console.error(err)
         return res.status(500).send(
             {
                 success: false,
-                message: 'General error with register admin'
+                message: 'General error whit default admin',
+                err
             }
         )
     }
 }
 
+export const changeRole = async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+
+        if (!user) {
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'User not found'
+                }
+            )
+        }
+
+        if (user.role === 'ADMIN') {
+            return res.status(401).send(
+                {
+                    success: false,
+                    message: 'Admins cannot be changed'
+                }
+            )
+        }
+
+        const newAdmin = await User.findByIdAndUpdate(
+            id,
+            { role: 'ADMIN' },
+            { new: true }
+        )
+
+        res.status(200).send(
+            {
+                success: true,
+                message: 'User updated to Admin',
+                newAdmin
+            }
+        )
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error whit Changing role',
+                err
+            }
+        )
+    }
+}
