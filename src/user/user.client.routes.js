6738e9f1b3aedd3789187707b4105f21efc0fb02
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-27 22:57:00 -0600

    HTTP methods updated

--- src/user/user.client.routes.js
+++ src/user/user.client.routes.js
@@ -1,27 +1,21 @@
 import { Router } from "express";
-import { deleteAccount, registerUser, updateProfile } from "./user.client.controller.js";
-import { registerValidator } from "../../middlewares/validators.js";
+import { deleteAccount, updateProfile } from "./user.client.controller.js";
 import { validateJwt } from "../../middlewares/validate.jwt.js";
 
 const api = Router()
 
 //Ruta Públicas
 
-api.post(
-    '/Register',
-    [registerValidator],
-    registerUser
-)
 
 api.delete(
-    '/',
+    '/:password',
     [validateJwt],
     deleteAccount
 )
 
 
 api.put(
-    '/',
+    '/:password',
     [validateJwt],
     updateProfile
 )
