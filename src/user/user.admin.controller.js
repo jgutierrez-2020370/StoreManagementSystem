085d2810b8f0318d516978887f828613a90f82b0
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-03-02 21:19:56 -0600

    Update and delete user added

--- src/user/user.admin.controller.js
+++ src/user/user.admin.controller.js
@@ -1,6 +1,7 @@
 import User from '../user/user.model.js'
 import { encrypt } from '../../utils/encrypt.js'
 import Cart from '../shoppingCart/shoppingCart.model.js'
+import { checkPassword } from '../../utils/encrypt.js'
 
 export const defaultAdmin = async(req, res) => {
     try {
@@ -84,6 +85,105 @@ export const changeRole = async (req, res) => {
                 success: false,
                 message: 'General error whit Changing role',
                 err
+            }
+        )
+    }
+}
+
+
+export const deleteUserAccount = async (req, res) => {
+    const { id } = req.params
+    try {
+        const user = await User.findById(id)
+
+        if(user.status === false) {
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'User not found'
+                }
+            )
+        }
+
+        if(user.role == "ADMIN") {
+            return res.status(401).send(
+                {
+                    success: false,
+                    message: 'Admins cannot be deleted'
+                }
+            )
+        }
+
+        await User.findByIdAndUpdate(
+            user, 
+            { status: false }, 
+            { new: true }
+        )
+
+        return res.send(
+            {
+                success: true,
+                message: 'User account deleted successfully'
+            }
+        )
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error deleting account'
+            }
+        )
+    }
+}
+
+export const updateUserProfile = async (req, res)=> {
+    const { id } = req.params
+    const { ...data } = req.body
+    try {
+        const user = await User.findById(id)
+
+        if(user.status === false) {
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'User not found'
+                }
+            )
+        }
+
+        if(user.role == "ADMIN") {
+            return res.status(401).send(
+                {
+                    success: false,
+                    message: 'Admins cannot be updated'
+                }
+            )
+        }
+
+        const userUpadted = await User.findByIdAndUpdate(
+            id,
+            data,
+            { new: true }
+        ).populate('cart', 'items total')
+
+        return res.send(
+            {
+                success: true,
+                message: 'User updated successfully',
+                userUpadted
+            }
+        )
+
+
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error updating account'
             }
         )
     }
