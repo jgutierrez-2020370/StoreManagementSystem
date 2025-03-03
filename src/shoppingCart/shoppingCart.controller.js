018a4d95ab2755561d51614ed0af957e656883fe
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-03-02 21:18:43 -0600

    Function deleteProductCart added

--- src/shoppingCart/shoppingCart.controller.js
+++ src/shoppingCart/shoppingCart.controller.js
@@ -110,7 +110,6 @@ export const getMyCart = async (req, res) => {
                 }
             }
         )
-
         if(!cart){
             res.status(404).send(
                 {
@@ -133,6 +132,63 @@ export const getMyCart = async (req, res) => {
             {
                 success: false,
                 message: 'General error when getting the cart'
+            }
+        )
+    }
+}
+
+export const deleteProductCart = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const user = await User.findById(req.user.uid)
+        const cart = await Cart.findById(user.cart)
+
+        const product = await Product.findById(id)
+
+        const alreadyExist = cart.items.find(item => item.product.toString() === product._id.toString())
+
+        if (!alreadyExist) {
+            return res.status(400).send(
+                {
+                    success: false,
+                    message: 'the product is not in your cart'
+                }
+            )
+        }
+
+        cart.items.splice(alreadyExist, 1)
+
+        cart.total = cart.items.reduce((sum, item) => sum + item.price, 0)
+
+        await cart.save()
+
+        const updatedCart = await Cart.findById(cart._id).populate(
+            {
+                path: 'items.product',
+                select: 'name price description stock',
+                populate: {
+                    path: 'category',
+                    select: 'name -_id'
+                }
+            }
+        )
+
+        return res.status(200).send(
+            {
+                success: true,
+                message: 'Product removed successfully',
+                cart: updatedCart
+            }
+        )
+
+        
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error when deleting product from the car'
             }
         )
     }
